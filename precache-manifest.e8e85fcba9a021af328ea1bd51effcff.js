45a26bf7db2533530f4e2b58e70b28ab340e25d2
Yashpk789987 <43312758+Yashpk789987@users.noreply.github.com>
2019-02-18 01:25:49 +0530

    Updates

---
+++ precache-manifest.e8e85fcba9a021af328ea1bd51effcff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4ec20663964d5fe2fd89",
+    "url": "/AAA/static/js/runtime~main.4ec20663.js"
+  },
+  {
+    "revision": "5830785f9c4ec31c0794",
+    "url": "/AAA/static/js/main.5830785f.chunk.js"
+  },
+  {
+    "revision": "7f10bbff10e492b16095",
+    "url": "/AAA/static/js/2.7f10bbff.chunk.js"
+  },
+  {
+    "revision": "5830785f9c4ec31c0794",
+    "url": "/AAA/static/css/main.ab1faeff.chunk.css"
+  },
+  {
+    "revision": "7f10bbff10e492b16095",
+    "url": "/AAA/static/css/2.5623f183.chunk.css"
+  },
+  {
+    "revision": "2be8d71b75e68b9967ed7998d2f5ffd6",
+    "url": "/AAA/index.html"
+  }
+];
